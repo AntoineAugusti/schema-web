85fbc179195f501d3d4e8a37d195bdc7c5265cca
Antoine Augusti <antoine.augusti@gmail.com>
2019-05-04 19:34:54 +0200

    Copy schemas.yml file

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 wget https://github.com/AntoineAugusti/schema-agg/archive/master.zip -O /tmp/schema-agg.zip
 unzip /tmp/schema-agg.zip -d /tmp
 cp -r /tmp/schema-agg-master/data/. articles/_schemas/
+cp /tmp/schema-agg-master/data/schemas.yml _data/
 bundle exec jekyll build
