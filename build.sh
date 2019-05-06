5fd560d846ecd875479bac05c15ffa5e880e84fb
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-05-06 11:00:54 +0200

    Update build script with new folder name

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 wget https://github.com/AntoineAugusti/schema-agg/archive/master.zip -O /tmp/schema-agg.zip
 unzip /tmp/schema-agg.zip -d /tmp
-cp -r /tmp/schema-agg-master/data/. articles/_schemas/
+cp -r /tmp/schema-agg-master/data/. collections/_schemas/
 cp /tmp/schema-agg-master/data/schemas.yml _data/
 bundle exec jekyll build
