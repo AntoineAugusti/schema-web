0a82da7a12d34201aade4d244a246fbee36f8e69
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-05-07 11:33:36 +0200

    Include schemas.zip when building

--- build.sh
+++ build.sh
@@ -4,4 +4,9 @@ unzip /tmp/schema-agg.zip -d /tmp
 cp -r /tmp/schema-agg-master/data/. collections/_schemas/
 cp /tmp/schema-agg-master/data/schemas.yml _data/
 bundle exec jekyll build
+
+# Add headers' rules for Netlify
 cp _headers _site/
+
+# Zip the schemas folder and include it in the published website
+cd collections && mv _schemas schemas && zip -r schemas.zip schemas/ && mv schemas _schemas && mv schemas.zip ../_site/schemas/
